59f83237b333b6000735c4ebb1e7f20826aa45e8
AyushMasmare <ayushmasmare6@gmail.com>
2026-01-18 22:48:20 +0530

    Add files via upload

---
+++ practical-2.py
@@ -0,0 +1,15 @@
+temp=float(input("Enter temperature in degree Celsius: "))
+if temp < -273.15:
+    print("Invalid temperature: below absolute zero")
+elif temp==-273.15:
+    print("Temperature is absolute zero")
+elif -273.15<temp<0:
+    print("Temperature is below freezing")
+elif temp==0:
+    print("Temperature is at the freezing point")
+elif 0<temp<100:
+    print("Temperature is in the normal range")
+elif temp==100:
+    print("Temperature is at the boiling point")
+elif temp>100:
+    print("Temperature is above the boiling point")
